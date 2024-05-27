79e17e34a016a6e79c614c60bd5946316cdf380f
Junee <juneekim7@gmail.com>
2024-05-28 00:12:54 +0900

    nip io

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import cors from 'cors'
 import { configDotenv } from 'dotenv'
 import { RawData, WebSocket, WebSocketServer } from 'ws'
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb'
@@ -18,11 +17,6 @@ const app = express()
 app.use(express.json({
     limit: '10mb'
 }))
-const corsOptions = {
-    origin: 'http://13.125.104.45.nip.io/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-app.use(cors(corsOptions))
 ViteExpress.config({
     mode: 'production',
     inlineViteConfig: {
