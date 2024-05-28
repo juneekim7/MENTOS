b61f2b2478b9f2ac2600ce11ef55780b46ca973d
Junee <juneekim7@gmail.com>
2024-05-29 00:31:12 +0900

    한글 에러

--- server/src/mentoring.ts
+++ server/src/mentoring.ts
@@ -7,13 +7,13 @@ import { getRes } from './utils'
 export const getMentoring = getRes(async (code: number, user: User, auth: Auth = 'any') => {
     const mentoring = await mentoringColl().findOne({ code }, withoutId)
     if (mentoring === null) {
-        return failure(`No mentoring with the code ${code}`)
+        return failure(`코드 ${code}의 멘토링이 존재하지 않습니다.`)
     }
     if (auth === 'mentor' && !mentoring.mentors.map((u) => u.id).includes(user.id)) {
-        return failure('You are not the mentor of this mentoring!')
+        return failure('당신은 이 멘토링의 멘토가 아닙니다!')
     }
     if (auth === 'mentee' && !mentoring.mentees.map((u) => u.id).includes(user.id)) {
-        return failure('You are not the mentee of this mentoring!')
+        return failure('당신은 이 멘토링의 멘티가 아닙니다!')
     }
     return success(mentoring)
 })
@@ -22,28 +22,28 @@ export const checkLog = async (log: Partial<Log>) => {
     const { location, start, end, startImageId, endImageId } = log
     if (location) {
         if (location === '') {
-            return failure('Empty location')
+            return failure('장소가 비어있습니다.')
         }
     }
     if (start && end) {
         const duration = end.getTime() - start.getTime()
         if (duration <= 0) {
-            return failure('Duration should be positive')
+            return failure('시간은 양수여야 합니다.')
         }
         if (duration >= maxDuration) {
-            return failure(`Duration should be maximum ${maxDuration / (60 * 60 * 1000)} hours`)
+            return failure(`멘토링은 최대 ${maxDuration / (60 * 60 * 1000)} 시간만 가능합니다.`)
         }
     } else if (start || end) {
-        return failure('There must be start and end')
+        return failure('시작 시각과 종료 시각이 모두 존재해야 합니다.')
     }
     if (startImageId) {
         if (startImageId === '') {
-            return failure('Empty startImageId')
+            return failure('startImageId가 비어있습니다.')
         }
     }
     if (endImageId) {
         if (endImageId === '') {
-            return failure('Empty endImageId')
+            return failure('endImageId가 비어있습니다.')
         }
     }
     return success(null)
