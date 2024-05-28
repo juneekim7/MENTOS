b818e576049db40906e24f6e48d4a8f9698567ba
Junee <juneekim7@gmail.com>
2024-05-29 00:45:57 +0900

    build

--- client/src/server.config.ts
+++ client/src/server.config.ts
@@ -1,4 +1,4 @@
-// const ip = "ksamentos.kr"
-const ip = "localhost"
+const ip = "ksamentos.kr"
+// const ip = "localhost"
 export const SERVER_HOST = `http://${ip}`
 export const WEBSOCKET_HOST = `ws://${ip}:3000`
